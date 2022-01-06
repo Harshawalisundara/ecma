9b91ced4663afde177777b8c20cf6f5abc3f93b1
Harshawalisundara <19it537@itum.mrt.ac.lk>
2022-01-07 02:12:57 +0530

    i will add/add rout

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('/project',(req,res)=>{
     res.sendstatus(200)
 })
 
+//getmapping add two number
+
+app.get('/add/:num1/:num2',(req,res)=>{
+    const {num1,num2} = req.params
+    const ans =num1+num2;
+    res.send(ans)
+
+})
 //called app.listen method
 
 app.listen(port,()=>){
